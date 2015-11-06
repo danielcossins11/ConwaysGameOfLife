4ee224c61062339a68b3c467d2ed163385fbd627
danielcossins11 <daniel.cossins@yahoo.com>
2015-11-06 12:50:39 -0600

    added tests

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConwaysGameOfLife;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InitializeCell()
+        {
+            Cell cell = new Cell(0, 0);
+            Assert.IsNotNull(cell);
+        }
+    }
+}
